e5763c87170a4fc1b3e8abbb80792aea3292ff3c
Emanuel Imhof <emanuel@sahli.io>
2022-02-03 10:47:04 +0100

    enhancement - add pwa-capabilites with favicons

--- pages/index.js
+++ pages/index.js
@@ -38,8 +38,25 @@ export default function Home() {
         <div className={styles.container}>
             <Head>
                 <title>percy - Pecentage Calculator</title>
-                <meta name="description" content="Percentage Calculation for your training"/>
-                <link rel="icon" href="/favicon.ico"/>
+                <meta name="description" content="Percentage Calculation for your Training"/>
+                <meta name='viewport'
+                      content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover'/>
+                <meta name='application-name' content='percy'/>
+                <meta name='apple-mobile-web-app-capable' content='yes'/>
+                <meta name='apple-mobile-web-app-status-bar-style' content='default'/>
+                <meta name='apple-mobile-web-app-title' content='percy'/>
+                <meta name='description' content='Percentage Calculation for your Training'/>
+                <meta name='format-detection' content='telephone=no'/>
+                <meta name='mobile-web-app-capable' content='yes'/>
+                <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png"/>
+                <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png"/>
+                <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png"/>
+                <link rel="manifest" href="/icons/site.webmanifest"/>
+                <link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#c17cef"/>
+                <link rel="shortcut icon" href="/icons/favicon.ico"/>
+                <meta name="msapplication-TileColor" content="#ffffff"/>
+                <meta name="msapplication-config" content="/icons/browserconfig.xml"/>
+                <meta name="theme-color" content="#ffffff"/>
             </Head>
 
             <main className={styles.main}>
@@ -78,7 +95,7 @@ export default function Home() {
                     <div className={styles.card}>
                         <Input id="customPercentage" placeholder="Custom" type="number" width="4rem"
                                min={1} max={100} step={1} name="customPercentage" value={customPercentage}
-                               onChange={event => setCustomPercentage(event.target.value)} />
+                               onChange={event => setCustomPercentage(event.target.value)}/>
                         <h3>{customPercentage ? parseInt(weight / 100 * customPercentage) : '-'}</h3>
                     </div>
                 </div>
